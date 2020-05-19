202e82dda99165e0431a614f372fec2f50a9adba
Alan <aogarciag@gmail.com>
2020-05-19 09:24:57 -0500

    error handling s3
    error handling s3

--- fraud_similarity_utils/util_s3.py
+++ fraud_similarity_utils/util_s3.py
@@ -31,45 +31,70 @@ class S3:
     def download_file(cls,key):
         try:
             file=cls.s3.get_object(Bucket=cls.BUCKET, Key=key)
-        except:
+        except Exception as e:
+            print(e)
             raise S3Exception("s3_failed_download_file")
         return file
 
     @classmethod
     def pandas_to_s3(cls,df,key):
         # write DF to string stream
-        csv_buffer = io.StringIO()
-        df.to_csv(csv_buffer, index=False)
+        try:
+            csv_buffer = io.StringIO()
+            df.to_csv(csv_buffer, index=False)
 
-        # reset stream position
-        csv_buffer.seek(0)
-        # create binary stream
-        gz_buffer = io.BytesIO()
+            # reset stream position
+            csv_buffer.seek(0)
+            # create binary stream
+            gz_buffer = io.BytesIO()
 
-        # compress string stream using gzip
-        with gzip.GzipFile(mode='w', fileobj=gz_buffer) as gz_file:
-            gz_file.write(bytes(csv_buffer.getvalue(), 'utf-8'))
-            
-        # write stream to S3
-        cls.s3.put_object(Bucket=cls.BUCKET, Key=key, Body=gz_buffer.getvalue())
-
-    @classmethod
-    def s3_to_pandas(cls,key, header=None):
-
-        # get key using boto3 client
-        obj = cls.s3.get_object(Bucket=cls.BUCKET, Key=key)
-        gz = gzip.GzipFile(fileobj=obj['Body'])
-        
-        # load stream directly to DF
-        return pd.read_csv(gz, header=header, dtype=str)
+            # compress string stream using gzip
+            with gzip.GzipFile(mode='w', fileobj=gz_buffer) as gz_file:
+                gz_file.write(bytes(csv_buffer.getvalue(), 'utf-8'))
+                
+            # write stream to S3
+            cls.s3.put_object(Bucket=cls.BUCKET, Key=key, Body=gz_buffer.getvalue())
+        except Exception as e:
+            print(e)
+            raise S3Exception("s3_failed_upload_file")
 
     @classmethod
-    def s3_to_pandas_with_processing(cls, key, header=None):
-        
-        # get key using boto3 client
-        obj = cls.s3.get_object(Bucket=cls.BUCKET, Key=key)
-        gz = gzip.GzipFile(fileobj=obj['Body'])
+    def s3_to_pandas(cls,key, header=None,_type='csv'):
+        try:
+            # get key using boto3 client
+            obj = cls.s3.get_object(Bucket=cls.BUCKET, Key=key)
+            gz = gzip.GzipFile(fileobj=obj['Body'])            
+            # load stream directly to DF
+            return pd.read_csv(gz, header=header, dtype=str)
+        except Exception as e:
+            print(e)
+            try:
+                if _type=='csv':
+                    return pd.read_csv(io.BytesIO(obj['Body'].read()))
+                else:
+                    return pd.read_pickle(io.BytesIO(obj['Body'].read()))
+            except Exception as e:
+                print(e)
+                raise S3Exception("s3_failed_download_file")
+        return 
 
-        # replace some characters in incomming stream and load it to DF
-        lines = "\n".join([line.replace('?', ' ') for line in gz.read().decode('utf-8').split('\n')])
-        return pd.read_csv(io.StringIO(lines), header=None, dtype=str)
+    @classmethod
+    def s3_to_pandas_with_processing(cls, key, header=None,_type='csv'):
+        try:
+            # get key using boto3 client
+            obj = cls.s3.get_object(Bucket=cls.BUCKET, Key=key)
+            gz = gzip.GzipFile(fileobj=obj['Body'])
+
+            # replace some characters in incomming stream and load it to DF
+            lines = "\n".join([line.replace('?', ' ') for line in gz.read().decode('utf-8').split('\n')])
+            return pd.read_csv(io.StringIO(lines), header=header, dtype=str)
+        except Exception as e:
+            print(e)
+            try:
+                if _type=='csv':
+                    return pd.read_csv(io.BytesIO(obj['Body'].read()))
+                else:
+                    return pd.read_pickle(io.BytesIO(obj['Body'].read()))
+            except Exception as e:
+                print(e)
+                raise S3Exception("s3_failed_download_file")
